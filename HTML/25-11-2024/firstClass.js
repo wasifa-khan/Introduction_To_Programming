547561aaee7c94c22cd4f0767c609d9e5e6eacfe
Umra-Badar20 <83424343+wasifa-khan@users.noreply.github.com>
2024-11-29 14:07:01 +0500

    code

---
+++ HTML/25-11-2024/firstClass.js
@@ -0,0 +1,19 @@
+console.log("Wasfa Khan");
+console.log(2 + 2);
+console.log("Sum of two numbers is:" + (2 / 2));
+
+
+var name; //declare a variable
+var name = "Wasfa"; //Initializes a variable
+var age = 21;
+var address = "xyz";
+
+console.log(name);
+console.log(age);
+console.log(address);
+
+console.log(name + " " + age + " " + address);
+
+
+
+
---
+++ Javascript/Class01/script.js
@@ -0,0 +1,65 @@
+var student; //declare
+var student = "John Ducket"; //intialize
+console.log(student);
+
+// var flowerNameOf = "Rose"; //CamelCase
+// var country_name = "Pakistan";
+// var studentName = "vyun";
+// var 12laptop = "dell";
+// var #laptop = "bnmm";
+
+// var age = 34;
+// console.log(Age);
+
+// var fullName = "Wasfa Khan";
+// var fullName = "Eshal";
+// var fullName = "Hello";
+
+
+// console.log(fullName);
+
+let fruitName1 = "Mango";
+// let fruitName1 = "Banana";
+fruitName1 = "Banana";
+let fruitName2 = "Orange";
+let fruitName3 = "Peach";
+
+console.log(fruitName1);
+console.log(fruitName2);
+//console.log(fruitName3);
+
+/*const country = "Pakistan";
+console.log(country);
+const PI = 3.142;
+console.log(PI);*/
+
+let age = "21";
+let dataOfBirth = "21-09-2005";
+let phoneNumber = 6731872919021037;
+let isNotFollow = false ;
+let jewllery;
+let city = null;
+
+console.log(typeof(age));
+console.log(typeof(dataOfBirth));
+console.log(typeof(phoneNumber));
+console.log(typeof(isNotFollow));
+console.log(jewllery);
+console.log(typeof(city));
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
