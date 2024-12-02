9191f9d9c50624c1cb9789f48769452cb8629a83
Umra-Badar20 <83424343+wasifa-khan@users.noreply.github.com>
2024-12-02 16:03:23 +0500

    code

---
+++ Javascript/Class02/script.js
@@ -0,0 +1,42 @@
+// let username; //declare
+// let myName = "Wasfa Khan";
+
+// let message = "Hello World";
+// alert(message)
+// console.log(message);
+
+// let studentName = "John";
+// let fatherName = "Ducket";
+// let phNumber = 57321910334;
+// let email = "xyz@gmail.com";
+// let studentAge = 17;
+// let rollNumber = "WMA-2572";
+
+// alert("Student name is:" + studentName);
+// alert(fatherName);
+// alert(phNumber);
+// alert(email);
+// alert(studentAge);
+// alert(rollNumber);
+
+
+// alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
+
+// let email = "example@example.com";
+// alert("My email address is: " + email)
+
+
+// document.write("Yah! I can write HTML content in Javascript");
+// console.log("Yah! I can write HTML content in Javascript");
+
+
+// let design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(design)
+// document.write(design)
+
+
+let num1 = 4;
+let num2 = 2;
+console.log("The sum of two number is : " + ((num1) % (num2)));
+
+
---
+++ Javascript/Class03/script.js
@@ -0,0 +1,57 @@
+let num1 = 16;
+let num2 = 46;
+console.log("The subtract of num1 and num2 is:" + (num1 - num2));
+console.log("The Multiple of num1 and num2 is:" + (num1 * num2));
+console.log("The divide of num1 and num2 is:" + (num1 / num2));
+console.log("The subtract of num1 and num2 is:" + (num1 % num2));
+console.log("The Exponent of num1 and num2 is:" + (num1 ** 5));
+
+let num3 = 12;
+// num3 -= 1; 
+num3 = num3 + 3; //17
+console.log(num3); 
+
+let a = 23;
+let b = 23; 
+console.log(a !== b); 
+
+console.log(a < b);
+
+
+let mode = 'black';
+if (mode === 'Orange') {
+    document.write("Dark mode is on");
+    document.body.style.backgroundColor = 'black';
+    document.body.style.color = 'white';
+   
+} else {
+    document.write("Light Mode is on");
+    document.body.style.backgroundColor = 'white';
+    document.body.style.color = 'black';
+}
+
+
+// let age = prompt("Enter your Age");
+// if(age < 18){
+//     console.log("You are eligible for voting");
+    
+// }
+// else if(age < 30){
+//     console.log("You are eligible ");
+    
+// }
+// else{
+//     console.log("You are not eligible, Try Later!");
+    
+// }
+
+let c = prompt("Enter the your email");
+// let d = prompt("Enter the number");
+
+console.log(c);
+
+
+
+// 
+
+
